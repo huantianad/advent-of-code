cd3b1b1d6c48836fae1f257d7ebfbe31dad068e2
huantian <davidtianli@gmail.com>
2021-12-04 13:38:11 -0700

    Make this much nicer

--- 2021/day4.nim
+++ 2021/day4.nim
@@ -1,104 +1,58 @@
 include aoc
 
-let testInput = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+type Board = seq[seq[int]]
 
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7"""
-
-type
-  Board = tuple
-    values: seq[seq[int]]
-
-proc newBoard(rawInput: string): Board =
-  let values = collect:
+proc initBoard(rawInput: string): Board =
+  collect:
     for row in rawInput.split("\n"): collect:
       for value in row.split:
         if not value.isEmptyOrWhitespace:
           parseInt(value)
 
-  return (values: values)
-
 proc hasWon(board: Board, passedTurns: seq[int]): bool =
-  for row in board.values:
-    if row.allit(it in passedTurns):
-      return true
+  for row in board:
+    if row.allit(it in passedTurns): return true
 
   for colNum in 0..4:
     let col = collect:
-      for row in board.values: row[colNum]
+      for row in board: row[colNum]
 
     if col.allit(it in passedTurns): return true
+
+proc getScore(board: Board, passedTurns: seq[int]): int =
+  let unmarkedValues = collect:
+    for row in board:
+      for value in row:
+        if value notin passedTurns: value
+
+  unmarkedValues.sum * passedTurns[^1]
 
 day 4:
   [rawTurns, *rawBoards] <- input.split("\n\n")
 
   let turns = rawTurns.split(",").map(parseInt)
-  let boards = collect:
-    for rawBoard in rawBoards:
-      newBoard(rawBoard)
+  let boards = rawBoards.map(initBoard)
 
-  var winner: Board
-  var hasWon: bool
-  var mostRecentIndex: int
-
-  # for turnIndex in 0..<turns.len:
-  #   for board in boards:
-  #     if board.hasWon(turns[0..turnIndex]):
-  #       winner = board
-  #       hasWon = true
-  #       mostRecentIndex = turnIndex
-  #       break
-
-  #   if hasWon: break
-
-  # let winningValues = collect:
-  #   for row in winner.values:
-  #     for value in row: value
-
-  # let unmarked = winningValues.filterIt(it notin turns[0..mostRecentIndex])
-  # let winningValue = turns[mostRecentIndex]
-
-  var hasNotWonBoardIndexes = (0..<boards.len).toSeq.toHashSet
+  var winners = initOrderedTable[Board, int]()
 
   for turnIndex in 0..<turns.len:
-    for boardIndex, board in boards.pairs:
-      if board.hasWon(turns[0..turnIndex]):
-        hasNotWonBoardIndexes.excl(boardIndex)
-        if board == winner:
-          mostRecentIndex = turnIndex
-          hasWon= true
-          break
+    let passedTurns = turns[0..turnIndex]
 
-      if hasNotWonBoardIndexes.len == 1: winner = boards[hasNotWonBoardIndexes.pop]
-      if hasWon: break
+    for board in boards:
+      if board.hasWon(passedTurns) and board notin winners:
+        winners[board] = board.getScore(passedTurns)
 
-    if hasNotWonBoardIndexes.len == 1: winner = boards[hasNotWonBoardIndexes.pop]
-    if hasWon: break
+    if winners.len == boards.len: break
 
-
-  let winningValues = collect:
-    for row in winner.values:
-      for value in row: value
-
-  let unmarked = winningValues.filterIt(it notin turns[0..mostRecentIndex])
-  let winningValue = turns[mostRecentIndex]
+  var firstScore: int
+  var lastScore: int
+  for score in winners.values:
+    firstScore = score
+    break
+  for score in winners.values:
+    lastScore = score
 
   part 1:
-    winningValue * sum(unmarked)
+    firstScore
   part 2:
-    winningValue * sum(unmarked)
+    lastScore
