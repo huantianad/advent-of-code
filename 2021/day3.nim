53a733887b630912301591036882e92e433a7ea9
huantian <davidtianli@gmail.com>
2021-12-03 18:54:30 -0700

    Make it better!

--- 2021/day3.nim
+++ 2021/day3.nim
@@ -1,54 +1,55 @@
 include aoc
 
-
-proc mostFrequent(input: seq[int]): int =
-  let numZero = input.count(0)
-  let numOne = input.count(1)
-
-  if numOne >= numZero: 1
-  else: 0
-
 proc switchZeroAndOne(input: int): int =
   if input == 1: 0 else: 1
 
-proc switchZeroAndOne(input: openArray[int]): seq[int] =
-  collect:
-    for element in input:
-      switchZeroAndOne(element)
+proc switchZeroAndOne(input: char): char =
+  if input == '1': '0' else: '1'
+
+proc switchZeroAndOne(input: string): string =
+  collect(
+    for bit in input:
+      bit.switchZeroAndOne
+  ).join
+
+proc switchZeroAndOne[T: int | char | string](input: openArray[T]): seq[T] =
+  input.map(switchZeroAndOne)
+
+proc mostFrequent(input: seq[int], invert = false): int =
+  let numZero = input.count(0)
+  let numOne = input.count(1)
+
+  if numOne >= numZero xor invert: 1 else: 0
 
 
 day 3:
-  proc solvePart2(input: seq[seq[int]], invert: bool): int =
-    var ignored = initOrderedSet[int]()
+  proc solvePart2(cols: seq[seq[int]], invert: bool): int =
+    var output: string
 
-    for colNum, col in input.pairs:
+    for colNum, col in cols.pairs:
       let remainingBits = collect:
-        for rowNum, bit in col.pairs:
-          if not ignored.contains(rowNum): bit
+        for line in lines:
+          if line.startsWith(output): parseInt($line[colNum])
 
       if remainingBits.len == 1: break
 
-      var most = mostFrequent(remainingBits)
-      if invert: most = most.switchZeroAndOne
+      output &= $mostFrequent(remainingBits, invert)
 
-      for rowNum, bit in col.pairs:
-        if bit != most:
-          ignored.incl rowNum
-
-    for rowNum, val in lines.pairs:
-      if not ignored.contains rowNum:
-        return fromBin[int](val)
+    for line in lines:
+      if line.startsWith(output):
+        return parseBinInt(line)
 
   let cols = collect:
     for colNum in 0..<lines[0].len: collect:
       for line in lines: parseInt($line[colNum])
 
   part 1, int:
-    let gammaArray = cols.map(mostFrequent)
+    let gammaArray = cols.mapIt(mostFrequent(it)).join
 
-    let gamma = fromBin[int](gammaArray.join)
-    let epsilon = fromBin[int](gammaArray.switchZeroAndOne.join)
+    let gamma = parseBinInt(gammaArray)
+    let epsilon = parseBinInt(gammaArray.switchZeroAndOne)
 
     gamma * epsilon
+
   part 2:
     solvePart2(cols, invert = false) * solvePart2(cols, invert = true)
