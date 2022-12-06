ae72ac04acffb80a63f14d6b778938bf27042024
huantian <davidtianli@gmail.com>
2022-12-06 15:40:31 -0700

    day5 updates

--- 2022/day5.nim
+++ 2022/day5.nim
@@ -4,12 +4,16 @@ type Move = object
   n, start, finish: Natural
 
 day 5:
-  var stacks: array[0..8, seq['A'..'Z']]
-  for x in 0..8:
-    for y in countdown(7, 0):
-      if lines[y][1 + (x * 4)] == ' ':
-        break
-      stacks[x].add lines[y][1 + (x * 4)]
+  let stacks = block:
+    var result: array[0..8, seq['A'..'Z']]
+
+    for x in 0..8:
+      for y in countdown(7, 0):
+        let maybeBox = lines[y][1 + (x * 4)];
+        if maybeBox == ' ': break
+        result[x].add maybeBox
+
+    result
 
   let moves = collect:
     for line in lines[10..^1]:
@@ -17,19 +21,23 @@ day 5:
       assert success
       Move(n: n, start: start - 1, finish: finish - 1)
 
-  # for move in moves:
-  #   for _ in 0..<move.n:
-  #     stacks[move.finish].add stacks[move.start].pop()
-
-  for move in moves:
-    for i in stacks[move.start][^(move.n)..^1]:
-      stacks[move.finish].add i
-    stacks[move.start].setLen(stacks[move.start].len - move.n)
-
   part 1:
-    var res = ""
-    for stack in stacks:
-      res.add stack[^1]
-    res
+    var stacks1 = stacks
+    for move in moves:
+      for _ in 0..<move.n:
+        stacks1[move.finish].add stacks1[move.start].pop()
+
+    result = ""
+    for stack in stacks1:
+      result.add stack[^1]
+
   part 2:
-    "Part 2 solution"
+    var stacks2 = stacks
+    for move in moves:
+      for i in stacks2[move.start][^(move.n)..^1]:
+        stacks2[move.finish].add i
+      stacks2[move.start].setLen(stacks2[move.start].len - move.n)
+
+    result = ""
+    for stack in stacks2:
+      result.add stack[^1]
