2dbee3a61e4c930421298f43791d9ef06c96dba0
huantian <davidtianli@gmail.com>
2022-12-08 15:33:23 -0700

    day8: done

--- 2022/day8.nim
+++ 2022/day8.nim
@@ -2,44 +2,39 @@ include aoc
 
 type Grid = seq[seq[int]]
 
-proc isVisible(grid: Grid, x, y: int): bool =
-  if all(collect(
-    for x2 in 0..<x:
-      grid[x][y] > grid[x2][y]
-  ), x => x):
-    return true
+proc calcScore(grid: Grid, x, y: int): tuple[score: int, visible: bool] =
+  var cleanSweeps = 4
+  result.score = 1
 
-  if all(collect(
-    for x2 in (x+1)..grid.high:
-      grid[x][y] > grid[x2][y]
-  ), x => x):
-    return true
+  template brr(grid: Grid, iter, exp: untyped) =
+    var i: int
+    for it in iter:
+      inc i
+      let it {.inject.} = it
+      if exp >= grid[x][y]:
+        cleanSweeps -= 1
+        break
+    result.score *= i
 
-  if all(collect(
-    for y2 in 0..<y:
-      grid[x][y] > grid[x][y2]
-  ), x => x):
-    return true
+  grid.brr(countdown(x - 1, grid.low), grid[it][y])
+  grid.brr(countup(x + 1, grid.high), grid[it][y])
+  grid.brr(countdown(y - 1, grid.low), grid[x][it])
+  grid.brr(countup(y + 1, grid.high), grid[x][it])
 
-  if all(collect(
-    for y2 in (y+1)..grid.high:
-      grid[x][y] > grid[x][y2]
-  ), x => x):
-    return true
+  result.visible = cleanSweeps > 0
 
 day 8:
   let grid = collect:
-    for line in lines:
-      line.mapIt(parseInt($it))
+    for line in lines: collect:
+      for c in line:
+        parseInt($c)
 
-  echo grid.len
-  echo grid.len * grid.len
-
-  part 1:
-    result = 0
+  let processed = collect:
     for x in 0..grid.high:
       for y in 0..grid.high:
-        if grid.isVisible(x, y):
-          inc result
+        grid.calcScore(x, y)
+
+  part 1:
+    processed.countIt(it.visible)
   part 2:
-    "Part 2 solution"
+    processed.mapIt(it.score).max()
