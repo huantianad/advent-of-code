e7ef856154d9c0694ab074980d3054d961aeebcf
huantian <davidtianli@gmail.com>
2022-12-13 12:37:41 -0700

    flake bump

--- flake.nix
+++ flake.nix
@@ -4,26 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    nimble.url = "github:nix-community/flake-nimble";
-    nimble.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, nimble }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ nimble.overlay ]; };
-      in
-      rec {
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; with pkgs.nimPackages; [
+          nativeBuildInputs = with pkgs; [
             openssl_1_1
             openssl
             nim
-            jsony
-            print
-            nimquery
-            benchy
           ];
         };
       }
